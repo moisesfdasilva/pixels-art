66e49f7512fae1460a0912db634c415f5afd5b82
moisesfdasilva <msesfernandes@gmail.com>
2022-07-06 20:32:57 -0300

    Realizados os requisitos 10 e 11, em index.html, style.css e script.js

--- script.js
+++ script.js
@@ -40,3 +40,56 @@ function clearTable() {
 }
 
 document.querySelector('#clear-board').addEventListener('click', clearTable);
+
+// REQUISITO 10:
+function clearToNewTable() {
+  const contador = document.querySelectorAll('.pixel').length - 1;
+  for (let element = contador; element >= 0; element -= 1) {
+    document.querySelectorAll('#pixel-board li')[element].remove();
+  }
+  const contador2 = document.querySelectorAll('#pixel-board br').length - 1;
+  for (let element = contador2; element >= 0; element -= 1) {
+    document.querySelectorAll('#pixel-board br')[element].remove();
+  }
+}
+
+function newTable(num) {
+  clearToNewTable();
+  for (let element = 0; element < (num * num); element += 1) {
+    const novoPixel = document.createElement('li');
+    const myBr = document.createElement('br');
+    if ((element + 1) % num === 0 && element > 0) {
+      document.querySelector('#pixel-board').appendChild(novoPixel);
+      document.querySelectorAll('#pixel-board li')[element].className = 'pixel';
+      document.querySelectorAll('.pixel')[element].insertAdjacentElement('afterend', myBr);
+    } else {
+      document.querySelector('#pixel-board').appendChild(novoPixel);
+      document.querySelectorAll('#pixel-board li')[element].className = 'pixel';
+    }
+  }
+}
+
+function verifyingfBSize0(newNum) {
+  if (newNum === 0) {
+    alert('Board inválido!');
+  } else {
+    return newTable(newNum);
+  }
+}
+
+function verifyingfBSize() {
+  const num = document.getElementById('board-size').value;
+  let newNum = 0;
+  if (num === '') {
+    newNum = 0;
+  } else if (num < 5) {
+    newNum = 5;
+  } else if (num > 50) {
+    newNum = 50;
+  } else {
+    newNum = num;
+  }
+  return verifyingfBSize0(newNum);
+}
+
+document.querySelector('#generate-board').addEventListener('click', verifyingfBSize);
