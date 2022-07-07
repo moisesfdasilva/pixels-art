6fb9e10f63120ace568378a094fb333ae58230f0
moisesfdasilva <msesfernandes@gmail.com>
2022-07-06 22:15:47 -0300

    Realizado o requisito 12, em index.html, style.css e script.js

--- script.js
+++ script.js
@@ -2,26 +2,31 @@ function early() {
   const newColor = getComputedStyle(document.querySelector('.color')).backgroundColor;
   document.querySelector('.color').className = 'selected color';
   document.querySelector('.selected').style.backgroundColor = newColor;
+  // let aleatoryColor = rgb(255, 255, 2);
+  for (let element = 1; element <= 3; element += 1) {
+    const alColorA = [(Math.random() * 256), (Math.random() * 256), (Math.random() * 256)];
+    const alColorB = `${'rgb('}${alColorA[0]}${', '}${alColorA[1]}${', '}${alColorA[2]}${')'}`;
+    document.getElementsByClassName('color')[element].style.backgroundColor = alColorB;
+  }
 }
 
 window.onload = early;
 
+const label = document.querySelectorAll('#color-palette div');
 function labelClass(action) {
-  const label = document.querySelectorAll('#color-palette div');
   for (let element = 0; element < label.length; element += 1) {
     if (action.target === document.querySelectorAll('#color-palette div')[element]) {
       const newColor = getComputedStyle(action.target).backgroundColor;
       action.target.className = 'selected color';
       document.querySelector('.selected').style.backgroundColor = newColor;
     } else {
-      document.querySelectorAll('#color-palette div')[element].className = 'color';
+      label[element].className = 'color';
     }
   }
-  console.log(document.querySelectorAll('#color-palette div'));
 }
 
-for (let element = 0; element < document.querySelectorAll('#color-palette div').length; element += 1) {
-  document.querySelectorAll('#color-palette div')[element].addEventListener('click', labelClass);
+for (let element = 0; element < label.length; element += 1) {
+  label[element].addEventListener('click', labelClass);
 }
 
 function paintPixel(action) {
@@ -43,9 +48,10 @@ document.querySelector('#clear-board').addEventListener('click', clearTable);
 
 // REQUISITO 10:
 function clearToNewTable() {
-  const contador = document.querySelectorAll('.pixel').length - 1;
+  const labelLi = document.querySelectorAll('#pixel-board li');
+  const contador = labelLi.length - 1;
   for (let element = contador; element >= 0; element -= 1) {
-    document.querySelectorAll('#pixel-board li')[element].remove();
+    labelLi[element].remove();
   }
   const contador2 = document.querySelectorAll('#pixel-board br').length - 1;
   for (let element = contador2; element >= 0; element -= 1) {
